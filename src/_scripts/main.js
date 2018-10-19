bd04110d31a7f15dca9d4a78f7bd58bc7bcd9a56
leeshuzheng <hadilee92@gmail.com>
2018-10-19 09:39:36 +0800

    added image assets, and gave background position x animation intervals

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -11,7 +11,17 @@ import Countdown from '../_modules/atoms/countdown/countdown';
 $(() => {
   window.end_date = new Date('11/11/2018');
 
-  console.log(`project end date is ${window.end_date}`);
-
   new Countdown(window.end_date);
+
+  // handle background
+  let body = $('body'),
+  xPos = 0;
+
+  let backgroundInterval = setInterval(function() {
+
+    xPos --;
+
+    body.css('background-position-x', `${xPos}px`);
+
+  }, 150); // every 10ms
 });
